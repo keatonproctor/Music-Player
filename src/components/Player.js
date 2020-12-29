6ab10f3aecc8f26e20750232e5641f49515bcd8a
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2020-12-29 15:08:44 -0700

    Added useRef for current audio selection

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 
-const Player = () => {
+const Player = ({ currentSong }) => {
+    //Ref
+    const audioRef = useRef(null);
+    //Event Handlers
+    const playSongHandler = () => {
+        console.log(audioRef.current);
+    };
     return(
         <div className="player">
             <div className="time-control">
@@ -14,9 +20,11 @@ const Player = () => {
 
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
+
+            <audio ref={audioRef} src={currentSong.audio}></audio>
         </div>
         
     )
