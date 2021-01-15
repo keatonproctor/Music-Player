a467e89ee947a4e17640817f3539b73322247a31
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2021-01-14 20:29:39 -0700

    Created autoplay for all songs if  playing/paused, changed title of app

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,9 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight, faPause } from '@fortawesome/free-solid-svg-icons';
 
 
-const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
-    //Ref
-    const audioRef = useRef(null);
+const Player = ({ audioRef, currentSong, isPlaying, setIsPlaying,setSongInfo, songInfo }) => {
     //Event Handlers
     const playSongHandler = () => {
         if(isPlaying){
@@ -15,12 +13,6 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             audioRef.current.play();
             setIsPlaying(!isPlaying);
         }
-    };
-    
-    const timeUpdateHandler = (e) => {
-        const current = e.target.currentTime;
-        const duration = e.target.duration;
-        setSongInfo({...songInfo, currentTime: current, duration})
     };
 
     const getTime = (time) => {
@@ -34,10 +26,6 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
         setSongInfo({...songInfo, currentTime: e.target.value});
     };
     //State
-    const [songInfo, setSongInfo] = useState({
-        currentTime: 0,
-        duration: 0,
-    });
 
     return(
         <div className="player">
@@ -58,14 +46,6 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
                 <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={isPlaying ? faPause : faPlay} />
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
             </div>
-
-            <audio 
-                onTimeUpdate={timeUpdateHandler}
-                onLoadedMetadata={timeUpdateHandler}
-                ref={audioRef} 
-                src={currentSong.audio}>
-            
-            </audio>
         </div>
         
     )
