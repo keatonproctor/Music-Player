c925229baeb610ff9639bfb8c9cfab30ba64fb7d
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2021-01-16 13:38:18 -0700

    Created song selection hover

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 
-const LibrarySong = ({song, songs, setCurrentSong, id, audioRef, isPlaying}) => {
+const LibrarySong = ({song, songs, setCurrentSong, id, audioRef, isPlaying, setSongs}) => {
     const songSelectHandler = () => {
         const selectedSong = songs.filter((state) => state.id ===id);
         setCurrentSong(selectedSong[0]);
+        //Add active state
+        const newSongs = songs.map((song) => {
+            if(song.id === id){
+                return{
+                    ...song,
+                    active: true,
+                }
+            }else{
+                return{
+                    ...song,
+                    active: false,
+                };
+            }
+        });
+        setSongs(newSongs);
+        
         //Check if Song is Playing
         if (isPlaying) {
             const playPromise = audioRef.current.play();
@@ -16,7 +32,7 @@ const LibrarySong = ({song, songs, setCurrentSong, id, audioRef, isPlaying}) => 
         }
     };
     return(
-        <div onClick={songSelectHandler} className="library-song">
+        <div onClick={songSelectHandler} className={`library-song ${song.active ? "selected" : ""}`}>
             <img alt={song.name} src={song.cover}></img>
             <div className="song-description">
                 <h3>{song.name}</h3>
