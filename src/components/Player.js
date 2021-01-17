9d125f82d3093f6c39b4f8c5b38a4b70171e2e35
Keaton Proctor <keatonproctor@Keatons-MacBook-Pro.local>
2021-01-17 11:59:23 -0700

    Active skipback and forward to match library song

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faAngleLeft, faAngleRight, faPause } from '@fortawesome/free-solid-svg-icons';
 
 
-const Player = ({audioRef, currentSong, setCurrentSong, isPlaying, setIsPlaying, setSongInfo, songInfo, songs}) => {
+const Player = ({audioRef, currentSong, setCurrentSong, isPlaying, setIsPlaying, setSongInfo, songInfo, songs, setSongs}) => {
+    //UseEffect
+    useEffect(() => {
+        const newSongs = songs.map((song) => {
+            if(song.id === currentSong.id){
+                return{
+                    ...song,
+                    active: true,
+                }
+            }else{
+                return{
+                    ...song,
+                    active: false,
+                };
+            }
+        });
+        setSongs(newSongs);
+    },[currentSong]);
     //Event Handlers
     const playSongHandler = () => {
         if(isPlaying){
